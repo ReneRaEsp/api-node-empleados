2031c7ceb6d6f31475f1edff145f7f66e1e9d664
ReneRaEsp <rdejramirez@gmail.com>
2021-07-27 21:36:51 -0500

    sirviendo

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from "express";
-const morgan = require("morgan");
-const cors = require("cors");
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
 const app=express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan("dev"));
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, ()=>{
 	console.log(`Server on port: ${PORT}`);
